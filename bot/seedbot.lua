7d2d8c681b777bf98475ae87851604af79737f97
focusworld <rezaabsy@yahoo.com>
2016-02-20 21:59:15 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,29 +226,24 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+ sudo_users = {179286096,157187288,119650184,174602122,4,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[focus v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@mamaligod [Founder]
+@mamaligodem [Developer]
+@xxx_sargardan_xxx [Developer]
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+amir
+sosha
+arian
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@focus_team [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
